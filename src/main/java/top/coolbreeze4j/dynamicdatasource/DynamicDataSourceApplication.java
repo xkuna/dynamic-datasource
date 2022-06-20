5149569ef91792b411893da9f04e150fab43b3bd
xkuna <1614651354@qq.com>
2022-06-20 18:57:02 +0800

    删除多余import

--- src/main/java/top/coolbreeze4j/dynamicdatasource/DynamicDataSourceApplication.java
+++ src/main/java/top/coolbreeze4j/dynamicdatasource/DynamicDataSourceApplication.java
@@ -5,14 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import top.coolbreeze4j.dynamicdatasource.test.service.TestService;
-import top.coolbreeze4j.dynamicdatasource.utils.SpringUtils;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableTransactionManagement
 @EnableAspectJAutoProxy(exposeProxy = true)
-@MapperScan(basePackages = {"top.coolbreeze4j.**.mapper"},sqlSessionTemplateRef = "sqlSessionTemplate")
+@MapperScan(basePackages = {"top.coolbreeze4j.**.mapper"})
 public class DynamicDataSourceApplication {
 
     public static void main(String[] args) {
